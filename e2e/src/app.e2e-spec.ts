3adcd538bee22e487cc41b59191269686a9af09c
Jens Winter <jens.winter@gmail.com>
2020-05-26 21:55:40 +0200

    Renamed "bluehendes-magdeburg" => "bluehwiesen-magdeburg"

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bluehendes-magdeburg!');
+    expect(page.getTitleText()).toEqual('Welcome to bluehwiesen-magdeburg!');
   });
 
   afterEach(async () => {
